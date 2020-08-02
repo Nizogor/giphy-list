4b209f3fa6d671800257a2ddbdd903d8438985a8
Nikita Teplyakov <>
2020-08-02 14:57:43 +0300

    add project

---
+++ Giphy List/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  Giphy List
+//
+//  Created by Nikita Teplyakov on 01.08.2020.
+//  Copyright © 2020 Nikita Tepliakov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@end
+
---
+++ Giphy List/AppDelegate.m
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.m
+//  Giphy List
+//
+//  Created by Nikita Teplyakov on 01.08.2020.
+//  Copyright © 2020 Nikita Tepliakov. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+	// Override point for customization after application launch.
+	return YES;
+}
+
+@end
